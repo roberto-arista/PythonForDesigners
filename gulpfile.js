cee89553cc2b01ec0319291327e32eb52f39de26
Roberto Arista <arista.rob@gmail.com>
2018-12-26 18:16:26 +0100

    autoprefixer setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 'use strict';
 
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 var browserSync = require('browser-sync').create();
 
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
 gulp.task('styles', function () {
-  return gulp.src('lib/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('assets/static/css'))
-    .pipe(browserSync.reload({
-      stream: true
-      }))
-});
+
+  return gulp
+      .src('lib/sass/**/*.scss')
+      .pipe(sass(sassOptions).on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('assets/static/css'))
+      .pipe(browserSync.reload({
+        stream: true
+        }));
+  });
 
 gulp.task('browserSync', function() {
   browserSync.init({
