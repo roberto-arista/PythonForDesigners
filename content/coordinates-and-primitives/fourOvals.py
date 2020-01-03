e46adaf992c181e7e602a262e2439431967e779b
Roberto Arista <arista.rob@gmail.com>
2020-01-03 15:37:05 +0100

    exercises restructured + new exercises

---
+++ content/coordinates-and-primitives/fourOvals.py
@@ -0,0 +1,8 @@
+newPage(100, 100)
+
+radius = 10
+
+oval(-radius, -radius, radius*2, radius*2)
+oval(width()-radius, -radius, radius*2, radius*2)
+oval(width()-radius, height()-radius, radius*2, radius*2)
+oval(-radius, height()-radius, radius*2, radius*2)
---
+++ content/coordinates-and-primitives/fourSquares.py
@@ -0,0 +1,7 @@
+newPage(100, 100)
+
+side = 20
+rect(0, 0, side, side)
+rect(width()-side, 0, side, side)
+rect(width()-side, height()-side, side, side)
+rect(0, height()-side, side, side)
