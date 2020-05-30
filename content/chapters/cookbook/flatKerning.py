11559324ed2d7bee41f9cfdbb7ad9eb8a6a189c2
Roberto Arista <hello@robertoarista.it>
2020-05-30 12:00:52 +0200

    csv with keys and flat kerning fixed, BenKiel suggestion

--- content/chapters/cookbook/flatKerning.py
+++ content/chapters/cookbook/flatKerning.py
@@ -1,36 +1,9 @@
 ### Modules
-from collections import defaultdict
-
-### Constants
-FIRST_PREFIX = 'public.kern1.'
-SECOND_PREFIX = 'public.kern2.'
+from fontParts.world import OpenFont
 
 ### Functions
-def flatKerning(aFont):
-    flatK = defaultdict(int)
-    flatK.setdefault(0)
-
-    for pair, correction in aFont.kerning.items():
-        first, second = pair
-
-        # both groups
-        if first.startswith(FIRST_PREFIX) and second.startswith(SECOND_PREFIX):
-            for firstGlyph in aFont.groups[first]:
-                for secondGlyph in aFont.groups[second]:
-                    flatK[(firstGlyph, secondGlyph)] = correction
-
-        # first group, second glyph
-        elif first.startswith(FIRST_PREFIX):
-            for firstGlyph in aFont.groups[first]:
-                flatK[(firstGlyph, second)] = correction
-
-        # first glyph, second group
-        elif second.startswith(SECOND_PREFIX):
-            for secondGlyph in aFont.groups[second]:
-                flatK[(first, secondGlyph)] = correction
-
-        # first glyph, second glyph
-        else:
-            flatK[(first, second)] = correction
-
-    return flatK
+if __name__ == '__main__':
+    ff = OpenFont('someFont.ufo')
+    flatKerning = ff.getFlatKerning()
+    print(flatKerning)
+    # {('A', 'V'): -80, ('Agrave', 'V'): -80, ('Aacute', 'V'): -80}
--- content/chapters/cookbook/loadTableWithKeys.py
+++ content/chapters/cookbook/loadTableWithKeys.py
@@ -1,19 +1,15 @@
 import csv
 
 def loadTableWithKeys(filePath, delimiter, quotechar):
-    table = []
     with open(filePath, mode='r', encoding='utf-8') as csvFile:
-        tableReader = csv.reader(csvFile, delimiter=delimiter, quotechar=quotechar)
-        for indexRow, eachRow in enumerate(tableReader):
-            if indexRow == 0:
-                keys = eachRow
-            else:
-                table.append({kk: vv for (kk, vv) in zip(keys, eachRow)})
-    return table
+        tableReader = csv.DictReader(csvFile, delimiter=delimiter, quotechar=quotechar)
+        return list(tableReader)
 
 
 if __name__ == '__main__':
     table = loadTableWithKeys('table.csv', '\t', '|')
-    # [{'city': 'Shanghai', 'country': 'China'},
-    #  {'city': 'New York', 'country': 'United States'},
-    #  {'city': 'Roma', 'country': 'Italy'}]
+    # [
+    #     OrderedDict([('city', 'Shanghai'), ('country', 'China')]),
+    #     OrderedDict([('city', 'New York'), ('country', 'United States')]),
+    #     OrderedDict([('city', 'Roma'), ('country', 'Italy')])
+    # ]
