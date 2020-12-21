68bc061c090d7bc3831403e14eb6248abbcae971
Roberto Arista <hello@robertoarista.it>
2020-12-21 12:23:38 +0100

    maybe we should automate export of png from pdfs in workbook (at least)

--- content/chapters/workbook/biggerImage.py
+++ content/chapters/workbook/biggerImage.py
@@ -1,1 +1,8 @@
-biggerImage.py
+fileName = 'protractor.pdf'
+scalingFactor = 4
+
+wdt, hgt = imageSize(fileName)
+newPage(wdt*scalingFactor, hgt*scalingFactor)
+scale(scalingFactor)
+image(fileName, (0, 0))
+saveImage('protractor.png')
