7a67864e8d3f88341cd754f46084074dd2929b11
Roberto Arista <hello@robertoarista.it>
2020-12-28 14:14:29 +0100

    renamed and improved

--- content/chapters/workbook/biggerImage.py
+++
@@ -1,8 +0,0 @@
-fileName = 'protractor.pdf'
-scalingFactor = 4
-
-wdt, hgt = imageSize(fileName)
-newPage(wdt*scalingFactor, hgt*scalingFactor)
-scale(scalingFactor)
-image(fileName, (0, 0))
-saveImage('protractor.png')
